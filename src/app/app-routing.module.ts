0b0f0520f4303e1da64a468afc1a106ccfc23327
IamFrost <nothing@nobody.nowhere>
2020-07-20 16:25:11 +0600

    signup design done, revision pending. routes added for signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
   {
     path:'',
-    redirectTo: 'login',
+    redirectTo: 'signup',
     pathMatch: 'full'
   },
   {
     path:'login',
     component: LoginComponent
+  },
+  {
+    path:'signup',
+    component: SignupComponent
   }
 ];
 
