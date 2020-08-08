96e67e5da27f51fd0d14d7a7f667d5de6d1309c6
IamFrost <nothing@nobody.nowhere>
2020-08-08 23:51:22 +0600

    code formatted

--- src/app/components/test2/test2.component.ts
+++ src/app/components/test2/test2.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { FormGroup, FormControl, Validators, FormBuilder, FormGroupName } from '@angular/forms'
 
 @Component({
   selector: 'app-test2',
@@ -7,56 +7,94 @@ import { FormGroup, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./test2.component.css']
 })
 export class Test2Component implements OnInit {
+  title = 'Reactive Forms';
+
+  powerCategoryVsPowerNameMap = new Map<string, Set<string>>();
+  heroCodeVsHeroNameMap = new Map<number, string>();
+  heroCodeVSPowerNameMap = new Map<number, Map<string, Set<string>>>();
+
+  accessFirstColumn: string[]= [];
+
+
+
 
 
   reactiveForm: FormGroup;
 
-  buildReactiveForm() {
-    const reactiveForm = new FormGroup({
-      firstname: new FormControl('', [Validators.required]),
-      lastname: new FormControl(''),
-      email: new FormControl(''),
-      address: new FormGroup({
-        address: new FormControl(''),
-        city: new FormControl(''),
-        state: new FormControl(''),
-      })
-    })
-  }
+  accessCheckboxFGN: FormGroupName;
 
-  setValue() {
+  constructor(private fb: FormBuilder) {
 
-    this.reactiveForm.setValue({
-      firstname: "Sachin",
-      lastname: "Tendulakr",
-      email: "sachin@gmail.com",
-      address: {
-        address: "19-A, Perry Cross Road, Bandra (West)",
-        city: "Mumbai",
-        state: "Maharatsra",
-      }
-    })
-  }
+    this.reactiveForm = new FormGroup({
+      firstname: new FormControl(false)
+    });
 
-  setAddress() {
-    this.reactiveForm.get("address").setValue({
-      address: "19-A, Perry Cross Road, Bandra (West)",
-      city: "Mumbai",
-      state: "Maharatsra",
-    })
-  }
-
-  onSubmit() {
-    console.log(this.reactiveForm.value);
-  }
-
-  constructor() {
+    
 
   }
+
   ngOnInit() {
+    this.setPowerCategoryVsPowerNameMap();
+    this.setAccessFirstColumn(this.powerCategoryVsPowerNameMap);
+    this.setHeroCodeVsHeroNameMap();
+    this.setHeroCodeVsPowerNameMap();
+  }
+  setHeroCodeVsHeroNameMap() {
+    this.heroCodeVsHeroNameMap.set(102, 'robin');
+    this.heroCodeVsHeroNameMap.set(556, 'alex');
+    this.heroCodeVsHeroNameMap.set(300, 'luke');
+  }
+  setPowerCategoryVsPowerNameMap() {
+    let powerSet = new Set<string>();
+    powerSet.add('shield');
+    powerSet.add('fire');
+    this.powerCategoryVsPowerNameMap.set('category-1', powerSet);
+    powerSet = new Set<string>();
+    powerSet.add('smoke');
+    powerSet.add('poison');
+    powerSet.add('ice');
+    this.powerCategoryVsPowerNameMap.set('category-2', powerSet);
+    powerSet = new Set<string>();
+    powerSet.add('self-heal');
+    powerSet.add('ice');
+    powerSet.add('fire');
+    this.powerCategoryVsPowerNameMap.set('category-3', powerSet);
+  }
+  setHeroCodeVsPowerNameMap() {
+    let powerSet = new Set<string>();
+    powerSet.add('smoke');
+    let powerMap = new Map<string, Set<string>>();
+    powerMap.set('category-2', powerSet);
+    powerSet = new Set<string>();
+    powerSet.add('ice');
+    powerMap = new Map<string, Set<string>>();
+    powerMap.set('category-3', powerSet);
+    this.heroCodeVSPowerNameMap.set(102, powerMap);
+    powerSet = new Set<string>();
+    powerSet.add('self-heal');
+    powerSet.add('ice');
+    powerMap = new Map<string, Set<string>>();
+    powerMap.set('category-3', powerSet);
+    this.heroCodeVSPowerNameMap.set(300, powerMap);
+  }
+  setAccessFirstColumn(mapInput: Map<string, Set<string>>) {
+    for(let entry of this.powerCategoryVsPowerNameMap.entries()){
+      this.accessFirstColumn.push(entry.toString());
+    }
+    return (mapInput.keys());
+  }
+  setAccessSecondColumn(firstColumnValue: string, mapInput: Map<string, Set<string>>) {
+    return (mapInput.get(firstColumnValue));
+  }
+
+  buildCheckBox(){
 
   }
 
+  uncheckAll() {
 
+  }
+  getAllValues() {
 
+  }
 }
--- src/app/components/test3/test3.component.ts
+++ src/app/components/test3/test3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 
 
 @Component({
@@ -8,7 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Test3Component implements OnInit {
 
+  title = 'Reactive Forms';
+
+  middleName: FormControl;
+
+  reactiveForm: FormGroup;
+
+  reactiveForm1: FormGroup;
+
   ngOnInit() {
+
+    this.reactiveForm = new FormGroup({
+      firstname: new FormControl(''),
+      lastname: new FormControl(''),
+      email: new FormControl(''),
+      address: new FormGroup({
+        address: new FormControl(''),
+        city: new FormControl(''),
+        state: new FormControl(''),
+      })
+    });
 
   }
 
