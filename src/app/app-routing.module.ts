055dbec1c5eb1863d62738d55732ea60490d81af
IamFrost <nothing@nobody.nowhere>
2020-11-19 10:58:20 +0600

    uncommented for testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,15 @@ import { Routes, RouterModule } from '@angular/router';
 // import { Test1Component } from './components/test/test1/test1.component';
 // import { Test4Component } from './components/test/test4/test4.component';
 
-import { LoginComponent } from '../app/components/login/login.component';
+import { LoginComponent } from '../app/components/test/login/login.component';
+import { Menu1Component } from './components/test/menu1/menu1.component';
+import { SelectBranchFyearComponent } from './components/test/select-branch-fyear/select-branch-fyear.component';
+import { SignupComponent } from './components/test/signup/signup.component';
+import { Test1Component } from './components/test/test1/test1.component';
+import { Test2Component } from './components/test/test2/test2.component';
+import { Test3Component } from './components/test/test3/test3.component';
+import { Test4Component } from './components/test/test4/test4.component';
+import { UserAccessComponent } from './components/test/user-access/user-access.component';
 
 const routes: Routes = [
   {
@@ -21,39 +29,39 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'selectbranchfyear',
+    component: SelectBranchFyearComponent
+  },
+  {
+    path: 't1',
+    component: Test1Component
+  },
+  {
+    path: 't2',
+    component: Test2Component
+  },
+  {
+    path: 't3',
+    component: Test3Component
+  },
+  {
+    path: 't4',
+    component: Test4Component
+  },
+  {
+    path: 'menu1',
+    component: Menu1Component
+  },
+  {
+    path: 'useraccess',
+    component: UserAccessComponent
   }
-  // {
-  //   path: 'signup',
-  //   component: SignupComponent
-  // },
-  // {
-  //   path: 'selectbranchfyear',
-  //   component: SelectBranchFyearComponent
-  // },
-  // {
-  //   path: 't1',
-  //   component: Test1Component
-  // },
-  // {
-  //   path: 't2',
-  //   component: Test2Component
-  // },
-  // {
-  //   path: 't3',
-  //   component: Test3Component
-  // },
-  // {
-  //   path: 't4',
-  //   component: Test4Component
-  // },
-  // {
-  //   path: 'menu1',
-  //   component: Menu1Component
-  // },
-  // {
-  //   path: 'useraccess',
-  //   component: UserAccessComponent
-  // }
 ];
 
 @NgModule({
